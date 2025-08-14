c28cfb692c40f822ea0b5af50be0380c7cc3231c
Natalie Spiva <natalie@acreetionos.org>
2025-08-14 06:08:02 -0700

    Updated DOCS them to dark...

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'  # Use ReadTheDocs theme
+html_theme = 'furo'  # Use ReadTheDocs theme
 
 html_static_path = ['_static']
 
